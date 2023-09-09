00ff4fcc5f7dd373a47f68b74e280d2eb5c9175b
minaxi <minaxijoshi3101@gmail.com>
2023-09-09 13:53:13 +0800

    navbar-fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ import "../styles/Navbar.css";
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <div className="leftSide">
             <a className="navbar-brand" href="/#">
