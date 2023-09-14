84ecc229003c8547c7573b4a81bc1e06bea8d30f
minaxi <minaxijoshi3101@gmail.com>
2023-09-15 03:03:17 +0800

    enroll me functionality implemented

--- src/components/Enrollnow.js
+++ src/components/Enrollnow.js
@@ -1,150 +1,393 @@
 import React from "react";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "../styles/EnrollNow.css";
 import user_icon from "../assets/images/EnrollNow/person.png";
 import email_icon from "../assets/images/EnrollNow/email.png";
 import password_icon from "../assets/images/EnrollNow/password.png";
 import mobile_icon from "../assets/images/EnrollNow/mobile.png";
+import axios from "../api/axios";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const EMAIL_REGEX = /\S+@\S+\.\S+/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const MOBILE_REGEX =
+  /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/gim;
+const REGISTER_URL = "/register";
 
 const Enrollnow = () => {
-  // States for registration
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [mobile, setMobile] = useState("");
+  const userRef = useRef();
+  const errRef = useRef();
 
-  // States for checking the errors
-  const [submitted, setSubmitted] = useState(false);
-  const [error, setError] = useState(false);
-  // Handling the name change
+  const [student_name, setName] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [student_email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [student_password, setPassword] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [student_mobile, setMobile] = useState("");
+  const [validMobile, setValidMobile] = useState(false);
+  const [mobileFocus, setMobileFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(student_name));
+  }, [student_name]);
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(student_email));
+  }, [student_email]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(student_password));
+    setValidMatch(student_password === matchPwd);
+  }, [student_password, matchPwd]);
+
+  useEffect(() => {
+    setValidMobile(MOBILE_REGEX.test(student_mobile));
+  }, [student_mobile]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [student_name, student_password, matchPwd]);
+
+  // Handling the student_name change
   const handleName = (e) => {
     setName(e.target.value);
-    setSubmitted(false);
   };
-  // Handling the email change
+  // Handling the student_email change
   const handleEmail = (e) => {
     setEmail(e.target.value);
-    setSubmitted(false);
   };
   // Handling the password change
   const handlePassword = (e) => {
     setPassword(e.target.value);
-    setSubmitted(false);
+  };
+
+  const handleMatchPassword = (e) => {
+    setMatchPwd(e.target.value);
   };
   const handleMobile = (e) => {
     setMobile(e.target.value);
-    setSubmitted(false);
   };
 
   // Handling the form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (name === "" || email === "" || password === "") {
-      setError(true);
-    } else {
-      setSubmitted(true);
-      setError(false);
+    const v1 = USER_REGEX.test(student_name);
+    const v2 = PWD_REGEX.test(student_password);
+    if (!v1 || !v2) {
+      setErrMsg("Invalid Entry");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        REGISTER_URL,
+        JSON.stringify({
+          student_name,
+          student_password,
+          student_mobile,
+          student_email,
+        }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      console.log(response?.data);
+      console.log(response?.accessToken);
+      console.log(JSON.stringify(response));
+      setSuccess(true);
+      //clear state and controlled inputs
+      //need value attrib on inputs for this
+      setName("");
+      setPassword("");
+      setMatchPwd("");
+      setEmail("");
+      setMobile("");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 409) {
+        setErrMsg("Username Taken");
+      } else {
+        setErrMsg("Registration Failed");
+      }
+      errRef.current.focus();
     }
   };
-  // Showing success message
-  const successMessage = () => {
-    return (
-      <div
-        className="success"
-        style={{
-          display: submitted ? "" : "none",
-        }}
-      >
-        <h4>User {name} successfully registered!!</h4>
-      </div>
-    );
-  };
-  // Showing error message if error is true
-  const errorMessage = () => {
-    return (
-      <div
-        className="error"
-        style={{
-          display: error ? "" : "none",
-        }}
-      >
-        <h4>Please enter all the fields</h4>
-      </div>
-    );
-  };
   return (
-    <div className="header">
-      <div className="text">Enroll me</div>
-      <h4>
-        Contact me below to find out more about how you can benefit me from
-        coaching.
-      </h4>
+    <>
+      {success ? (
+        <section>
+          <h4>User {userRef} successfully registered!!</h4>
+          <p>
+            <a href="www.google.com">Sign In</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <div className="header">
+            <div className="text">Enroll me</div>
+            <h4>
+              Contact me below to find out more about how you can benefit me
+              from coaching.
+            </h4>
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+            <form
+              name="enrollStudent"
+              id="enrollmentForm"
+              onSubmit={handleSubmit}
+            >
+              {/* Labels and inputs for form data */}
+              <div className="container">
+                <div className="underline">
+                  <div className="inputs">
+                    <div className="input">
+                      <img src={user_icon} alt="" />
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validName ? "valid" : "hide"}
+                      />
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        className={
+                          validName || !student_name ? "hide" : "invalid"
+                        }
+                      />
+                      <input
+                        id="username"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={handleName}
+                        value={student_name}
+                        type="text"
+                        placeholder="Name"
+                        name="student_name"
+                        required
+                        aria-invalid={validName ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setUserFocus(true)}
+                        onBlur={() => setUserFocus(false)}
+                      />
+                      <p
+                        id="uidnote"
+                        className={
+                          userFocus && student_name && !validName
+                            ? "instructions"
+                            : "offscreen"
+                        }
+                      >
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        4 to 24 characters.
+                        <br />
+                        Must begin with a letter.
+                        <br />
+                        Letters, numbers, underscores, hyphens allowed.
+                      </p>
+                    </div>
+                    <div className="input">
+                      <img src={email_icon} alt="" />
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validEmail ? "valid" : "hide"}
+                      />
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        className={
+                          validEmail || !student_email ? "hide" : "invalid"
+                        }
+                      />
+                      <input
+                        onChange={handleEmail}
+                        value={student_email}
+                        type="email"
+                        placeholder="Email"
+                        required
+                        aria-invalid={validEmail ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setEmailFocus(true)}
+                        onBlur={() => setEmailFocus(false)}
+                      />
+                      <p
+                        id="uidnote"
+                        className={
+                          emailFocus && student_email && !validEmail
+                            ? "instructions"
+                            : "offscreen"
+                        }
+                      >
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        Please enter a valid email address.
+                      </p>
+                    </div>
 
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
-      {/* Calling to the methods */}
-      <div className="messages">
-        {errorMessage()}
-        {successMessage()}
-      </div>
-
-      <form>
-        {/* Labels and inputs for form data */}
-        <div className="container">
-          <div className="underline">
-            <div className="inputs">
-              <div className="input">
-                <img src={user_icon} alt="" />
-                <input
-                  onChange={handleName}
-                  value={name}
-                  type="text"
-                  placeholder="Name"
-                />
-              </div>
-              <div className="input">
-                <img src={email_icon} alt="" />
-                <input
-                  onChange={handleEmail}
-                  value={email}
-                  type="email"
-                  placeholder="Email"
-                />
-              </div>
-              <div className="input">
-                <img src={password_icon} alt="" />
-                <input
-                  onChange={handlePassword}
-                  value={password}
-                  type="password"
-                  placeholder="Password"
-                />
-              </div>
-              <div className="input">
-                <img src={mobile_icon} alt="" />
-                <input
-                  onChange={handleMobile}
-                  value={mobile}
-                  type="text"
-                  placeholder="Mobile"
-                />
-              </div>
-              {/* <div className="forgot-password">
+                    <div className="input">
+                      <img src={password_icon} alt="" />
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validPwd ? "valid" : "hide"}
+                      />
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        className={
+                          validPwd || !student_password ? "hide" : "invalid"
+                        }
+                      />
+                      <input
+                        onChange={handlePassword}
+                        value={student_password}
+                        type="password"
+                        id="student_password"
+                        placeholder="Password"
+                        required
+                        aria-invalid={validPwd ? "false" : "true"}
+                        aria-describedby="pwdnote"
+                        onFocus={() => setPwdFocus(true)}
+                        onBlur={() => setPwdFocus(false)}
+                      />
+                      <p
+                        id="pwdnote"
+                        className={
+                          pwdFocus && !validPwd ? "instructions" : "offscreen"
+                        }
+                      >
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        8 to 24 characters.
+                        <br />
+                        Must include uppercase and lowercase letters, a number
+                        and a special character.
+                        <br />
+                        Allowed special characters:{" "}
+                        <span aria-label="exclamation mark">!</span>{" "}
+                        <span aria-label="at symbol">@</span>{" "}
+                        <span aria-label="hashtag">#</span>{" "}
+                        <span aria-label="dollar sign">$</span>{" "}
+                        <span aria-label="percent">%</span>
+                      </p>
+                    </div>
+                    <div className="input">
+                      <img src={password_icon} alt="" />
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validMatch && matchPwd ? "valid" : "hide"}
+                      />
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        className={validMatch || !matchPwd ? "hide" : "invalid"}
+                      />
+                      <input
+                        onChange={handleMatchPassword}
+                        id="confirm_pwd"
+                        type="password"
+                        placeholder="Confirm Password"
+                        aria-invalid={validMatch ? "false" : "true"}
+                        aria-describedby="confirmnote"
+                        onFocus={() => setMatchFocus(true)}
+                        onBlur={() => setMatchFocus(false)}
+                      />
+                      <p
+                        id="confirmnote"
+                        className={
+                          matchFocus && !validMatch
+                            ? "instructions"
+                            : "offscreen"
+                        }
+                      >
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        Must match the first password input field.
+                      </p>
+                    </div>
+                    <div className="input">
+                      <img src={mobile_icon} alt="" />
+                      <FontAwesomeIcon
+                        icon={faCheck}
+                        className={validMobile ? "valid" : "hide"}
+                      />
+                      <FontAwesomeIcon
+                        icon={faTimes}
+                        className={
+                          validMobile || !student_mobile ? "hide" : "invalid"
+                        }
+                      />
+                      <input
+                        onChange={handleMobile}
+                        value={student_mobile}
+                        type="text"
+                        placeholder="Mobile"
+                        required
+                        aria-invalid={validMobile ? "false" : "true"}
+                        aria-describedby="uidnote"
+                        onFocus={() => setMobileFocus(true)}
+                        onBlur={() => setMobileFocus(false)}
+                      />
+                      <p
+                        id="uidnote"
+                        className={
+                          mobileFocus && student_mobile && !validMobile
+                            ? "instructions"
+                            : "offscreen"
+                        }
+                      >
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        Please enter valid Mobile number with country code.
+                      </p>
+                    </div>
+                    {/* <div className="forgot-password">
                 Lost Password? <span>Click here</span>
               </div> */}
-              <div className="submit-container">
-                <div className="submit" onClick={handleSubmit}>
-                  Submit
+                    <div className="submit-container">
+                      {/* <div className="submit" onClick={handleSubmit}> */}
+                      <div className="submit" onClick={handleSubmit}>
+                        Submit
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </form>
+            <p>
+              Already registered?
+              <br />
+              <span className="line">
+                {/*put router link here*/}
+                <a href="www.google.com">Sign In</a>
+              </span>
+            </p>
           </div>
-        </div>
-      </form>
-    </div>
+        </section>
+      )}
+    </>
   );
 };
 
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,41 +11,38 @@ import {
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    // <footer>
-    //   <SocialIcon url="www.pinterest.com" className="youtube social">
-    //     <FontAwesomeIcon icon={faYoutube} size="2x" />
-    //   </SocialIcon>
-    //   <SocialIcon url="www.youtube.com" />
-    //   <SocialIcon url="www.instagram.com" />
-    //   <SocialIcon url="www.linkedin.com" />
-    //   <SocialIcon url="www.twitter.com" />
-    //   <SocialIcon url="www.github.com" />
-    //   {`Copyright © SuccessEduHub Code ${year}`}
-    // </footer>
     <div>
-      <a
-        href="https://www.youtube.com/@devopslab-livescenarios1302/videos"
-        className="youtube social"
-      >
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
-      <a
-        href="https://www.github.com/minaxijoshi3101/"
-        className="github social"
-      >
-        <FontAwesomeIcon icon={faGithub} size="2x" />
-      </a>
-      <a href="https://www.twitter.com/" className="twitter social">
-        <FontAwesomeIcon icon={faTwitter} size="2x" />
-      </a>
-      <a
-        href="https://www.instagram.com/minaxijoshi3101/"
-        className="instagram social"
-      >
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
-      <h5>{`Contact Us: queries@seh.com`}</h5>
-      <p> {`Copyright © SuccessEduHub Code ${year}`}</p>
+      <div className="container">
+        <ul className="d-flex flex-row justify-content-center align-items-center list-unstyled">
+          <a
+            href="https://www.youtube.com/@devopslab-livescenarios1302/videos"
+            className="youtube social"
+          >
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
+          <a
+            href="https://www.github.com/minaxijoshi3101/"
+            className="github social"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a href="https://www.twitter.com/" className="twitter social">
+            <FontAwesomeIcon icon={faTwitter} size="2x" />
+          </a>
+          <a
+            href="https://www.instagram.com/minaxijoshi3101/"
+            className="instagram social"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </ul>
+      </div>
+      <div className="container">
+        <h5>{`Query Us: queries@seh.com`}</h5>
+      </div>
+      <div className="container">
+        <p> {`Copyright © SuccessEduHub Code ${year}`}</p>
+      </div>
     </div>
   );
 };
